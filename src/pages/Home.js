66aeaccc9ba82dae10348edc083c8c0c67b16a10
matthew@matthewyak.com <matthew@matthewyak.com>
2022-11-07 14:54:15 -0800

    Update styles on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import { Navigate } from "react-router-dom";
 
+import styles from './Home.module.css'
+
 function Home() {
   if (!localStorage.getItem("country")) {
     return <Navigate to="/select-country" />;
@@ -7,18 +9,18 @@ function Home() {
     return (
       <div>
         <h1>$1K</h1>
-        <button className="financesBtn">$</button>
-        <button className="policiesBtn">All Policies</button>
-        <button className="lawOrderBtn">Law And Order</button>
-        <button className="cabBtn">Cabnet</button>
-        <button className="sitBtn">Situations</button>
-        <button className="secBtn">Security</button>
-        <button className="conBtn">Congress</button>
-        <p className="points">x64</p>
-        <div className="foreignPoliciesContainer"></div>
-        <div className="taxPoliciesContainer"></div>
-        <div className="lawOrderPoliciesContainer"></div>
-        <div className="econTransPoliciesContainer"></div>
+        <button className={styles["financesBtn"]}>$</button>
+        <button className={styles["policiesBtn"]}>All Policies</button>
+        <button className={styles["lawOrderBtn"]}>Law And Order</button>
+        <button className={styles["cabBtn"]}>Cabnet</button>
+        <button className={styles["sitBtn"]}>Situations</button>
+        <button className={styles["secBtn"]}>Security</button>
+        <button className={styles["conBtn"]}>Congress</button>
+        <p className={styles["points"]}>x64</p>
+        <div className={styles["foreignPoliciesContainer"]}></div>
+        <div className={styles["taxPoliciesContainer"]}></div>
+        <div className={styles["lawOrderPoliciesContainer"]}></div>
+        <div className={styles["econTransPoliciesContainer"]}></div>
       </div>
     );
   }
