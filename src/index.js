1eab46141a55d39a998375a244ca8bb69567d56f
Matthew Yak <matthew@matthewyak.com>
2022-11-06 21:06:41 -0800

    Added firebase support

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBI8e5tKyqcx9rw41UmhCtmugK7QTqL-gg",
+  authDomain: "real-politik-sim.firebaseapp.com",
+  projectId: "real-politik-sim",
+  storageBucket: "real-politik-sim.appspot.com",
+  messagingSenderId: "538419433322",
+  appId: "1:538419433322:web:4fcbf3a4b4a50bb45c9f13",
+  measurementId: "G-C94Y1S1SCJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
