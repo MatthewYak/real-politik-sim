b9ede0cc509212b200f25468f52c87cf3741cbee
RobloxEnderman <jwaayers5656@outlook.com>
2022-11-07 11:30:30 -0800

    Added btns

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 
 function Home() {
-  if (!localStorage.getItem('country')) {
-    return <Navigate to="/select-country" />
+  if (!localStorage.getItem("country")) {
+    return <Navigate to="/select-country" />;
   } else {
     return (
       <div>
-        <h1>Home</h1>
-        <p>Welcome to {localStorage.getItem('country')}</p>
+        <h1>$1K</h1>
+        <button className="financesBtn">$</button>
+        <button className="policiesBtn">All Policies</button>
+        <button className="lawOrderBtn">Law And Order</button>
+        <button className="cabBtn">Cabnet</button>
+        <button className="sitBtn">Situations</button>
+        <button className="secBtn">Security</button>
+        <button className="conBtn">Congress</button>
+        <p className="points">x64</p>
+        <div className="foreignPoliciesContainer"></div>
+        <div className="taxPoliciesContainer"></div>
+        <div className="lawOrderPoliciesContainer"></div>
+        <div className="econTransPoliciesContainer"></div>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
