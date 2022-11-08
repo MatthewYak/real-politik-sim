4adb0ae91c6d43acb10c336929954a9e23e4f082
Matthew Yak <matthew@matthewyak.com>
2022-11-07 20:36:20 -0800

    Updated styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
   } else {
     return (
       <div>
-        <h1 className={styles['money']}>$1K</h1>
+        <h1 className={styles["money"]}>$1K</h1>
         <div className={styles["sideBtnsContainer"]}>
           <button className={styles["financesBtn"]}>$</button>
           <button className={styles["policiesBtn"]}>All Policies</button>
@@ -17,12 +17,24 @@ function Home() {
           <button className={styles["sitBtn"]}>Situations</button>
           <button className={styles["secBtn"]}>Security</button>
         </div>
-        <button className={styles['nextQuarterBtn']}>Next Quarter</button>
-        <div className={styles["points"]}><p>x64</p></div>
-        <div className={styles["foreignPoliciesContainer"]}></div>
-        <div className={styles["taxPoliciesContainer"]}></div>
-        <div className={styles["lawOrderPoliciesContainer"]}></div>
-        <div className={styles["econTransPoliciesContainer"]}></div>
+        <button className={styles["nextQuarterBtn"]}>Next Quarter</button>
+        <div className={styles["points"]}>
+          <p>x64</p>
+        </div>
+        <div className={styles['policyContainer']}>
+          <div className={styles["foreignPoliciesContainer"]}>
+            <h2>Foreign Policies</h2>
+          </div>
+          <div className={styles["taxPoliciesContainer"]}>
+            <h2>Tax Policies</h2>
+          </div>
+          <div className={styles["lawOrderPoliciesContainer"]}>
+            <h2>Law And Order Policies</h2>
+          </div>
+          <div className={styles["econTransPoliciesContainer"]}>
+            <h2>Economic And Transportation Policies</h2>
+          </div>
+        </div>
       </div>
     );
   }
